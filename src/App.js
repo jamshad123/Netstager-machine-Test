9be61fcd05a6148fda02bf6c5111c364730100d5
Jamshad T V <jamshuzi5@gmail.com>
2023-03-25 15:48:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import TodoList from './components/TodoList';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Details from './components/Details';
+import { createContext } from 'react';
+import AddTodo from './components/AddTodo';
 
+const newcontext = createContext();
 function App() {
+
+  const [array, setarray] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+     <newcontext.Provider value={{array, setarray}}>
+      <BrowserRouter>
+
+        <Routes>
+        <Route path="/" element={<TodoList/>}/>
+          <Route path="/addTodo" element={<AddTodo/>}/>
+          <Route path="/details/:id" element={<Details/>}/>
+        </Routes>
+      </BrowserRouter>
+      </newcontext.Provider>
+
+      
     </div>
   );
 }
 
 export default App;
+export {newcontext}
